e962f01e987910b358732be822e8b14e10bc6047
muhammedakb <muhammetakb68@gmail.com>
2022-01-13 00:27:20 +0300

    active & generally completed

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,16 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 const Dashboard = () => {
   const { logout } = useAuth();
+
+  // active nav element
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+
   return (
     <nav id="dashboard">
-      <Link to={"/weekly-schedule"}>Ana Sayfa</Link>
+      <Link
+        to={"/weekly-schedule"}
+        className={splitLocation[1] === "weekly-schedule" ? "active" : ""}
+      >
+        <span
+          className={`home-icon ${
+            splitLocation[1] === "weekly-schedule" ? "active" : ""
+          }`}
+        ></span>
+        Ana Sayfa
+      </Link>
       {/* <Link to={""}>Sınıflar</Link> */}
-      <Link to={"/profile"}>Profil</Link>
+      <Link
+        to={"/profile"}
+        className={splitLocation[1] === "profile" ? "active" : ""}
+      >
+        <span
+          className={`profile-icon ${
+            splitLocation[1] === "profile" ? "active" : ""
+          }`}
+        ></span>
+        Profil
+      </Link>
 
       <p className="logout" onClick={logout}>
         Çıkış yap
+        <span className="logout-icon"></span>
       </p>
     </nav>
   );
